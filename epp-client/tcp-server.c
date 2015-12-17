264b773868e24ed2c2835eb2511978b1cbec7195
John Cook <watfordjc@users.noreply.github.com>
2015-12-17 01:11:27 +0000

    Add clTRID generation

--- epp-client/tcp-server.c
+++ epp-client/tcp-server.c
@@ -11,8 +11,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+/* XML Libraries */
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+/* Time and Date */
+#include <time.h>
 
 void established_connection(int);
+void xml_parse(char *msg_data2);
+void GetclTRID(char *TAG, char *client, char *priority, char *registrantID, char *clTRID);
 
 void error(char *msg)
 {
@@ -61,10 +68,10 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		struct timeval timeout_recv;
-		timeout_recv.tv_sec = 5;
+		timeout_recv.tv_sec = 300; // 5
 		timeout_recv.tv_usec = 0;
 		struct timeval timeout_send;
-		timeout_send.tv_sec = 90;
+		timeout_send.tv_sec = 90; // 90
 		timeout_send.tv_usec = 0;
 
 		newsock_fd = accept(sock_fd, (struct sockaddr *) &client_addr, &client_addr_len);
@@ -130,40 +137,36 @@ void established_connection(int sock)
 	}
 	memcpy(msg_length.bytes, buffer, header_size);
 	msg_length.whole = ntohl(msg_length.whole);
+	if (msg_length.whole < header_size) {
+		fprintf(stderr,"Error: Data length less than header size.\n");
+		printf("--------------------------------------------------------------------------------\n");
+		exit(1);
+	}
 	msg_data_length = msg_length.whole - header_size;
-
-//	printf("Message octets: %d %d %d %d\n", msg_length.bytes[0], msg_length.bytes[1], msg_length.bytes[2], msg_length.bytes[3]);
-//	printf("Message length: %u\n", msg_length.whole);
-//	printf("Message data length: %u\n", msg_data_length);
 
 	bzero((char *) temp, buffer_size);
 	memcpy(temp, buffer + header_size, buffer_size - header_size);
-//	printf("Message: ");
 
 	/*
 	Make msg_data large enough to hold the entire message.
-	While we aren't currently storing the entire message in the array, it is there for when we do.
 	*/
 	char msg_data[msg_data_length - header_size + 1]; // With NUL.
-	bzero((char *) msg_data, msg_data_length - header_size + 1);
+	memset(msg_data, 0, sizeof(msg_data));
+
+	int read_chars;
 	/*
 	If "msg_data_length" is less than "buffer size chars (buffer size minus 1) plus header_size", then the buffer contains the entire data message.
 	*/
 	if (msg_data_length < buffer_size_chars - header_size)
 	{
-		memcpy(msg_data, temp, msg_data_length - header_size);
+		read_chars = msg_data_length - header_size;
 	}
 	/* Otherwise, it only contains the start of the message. */
 	else
 	{
-		memcpy(msg_data, temp, buffer_size_chars - header_size);
-		//strcat(msg_data, temp);
+		read_chars = buffer_size_chars - header_size;
 	}
-
-	/*
-	If the message contains a NUL character printf is not suitable here or in the while loop.
-	*/
-	printf("%s", msg_data);
+	memcpy(msg_data, temp, read_chars);
 
 	/* Position is how many bytes we have parsed. */
 	position = buffer_size_chars - header_size;
@@ -173,15 +176,20 @@ void established_connection(int sock)
 	while (position < msg_data_length)
 	{
 		remaining_chars = msg_data_length - position;
+		int read_chars;
 		bzero((char *) buffer, buffer_size);
+
 		if (remaining_chars > buffer_size_chars)
 		{
-			n = read(sock, buffer, buffer_size_chars);
+			read_chars = buffer_size_chars;
 		}
 		else if (remaining_chars > 0)
 		{
-			n = read(sock, buffer, remaining_chars);
+			read_chars = remaining_chars;
 		}
+
+		n = read(sock, buffer, read_chars);
+
 		if (errno == EAGAIN)
 		{
 			/* Connection closed due to read timeout. */
@@ -193,9 +201,13 @@ void established_connection(int sock)
 		{
 			error("subsequent read");
 		}
-		printf("%s", buffer);
+		memcpy(msg_data + position, buffer, read_chars);
 		position = position + n;
 	}
+
+	char *msgPtr;
+	msgPtr = msg_data;
+	xml_parse(msgPtr);
 
 	printf("\n--------------------------------------------------------------------------------\n");
 
@@ -206,4 +218,48 @@ void established_connection(int sock)
 	{
 		error("write");
 	}
+}
+
+void xml_parse(char *msg_data)
+{
+	/*
+	If the message contains a NUL character printf is not suitable here.
+	*/
+	printf("%s\n", msg_data);
+
+	char clTRID[64] = "";
+	bzero((char *) clTRID, 65);
+
+	char *TAG = "JOHNCOOK";
+	char *client = "00";
+	char *priority = "00";
+
+	char *registrantID = "123456";
+
+	GetclTRID(TAG, client, priority, registrantID, clTRID);
+	printf("clTRID: %s\n", &clTRID);
+
+}
+
+void GetclTRID(char *TAG, char *client, char *priority, char *registrantID, char *clTRID)
+{
+	struct timespec unixTime;
+	clock_gettime(CLOCK_REALTIME, &unixTime);
+
+	time_t epochSecs;
+	epochSecs = unixTime.tv_sec;
+
+	struct tm *dateTime;
+	dateTime = gmtime(&epochSecs);
+
+	memcpy(clTRID, TAG, strlen(TAG));
+	memcpy(clTRID + strlen(clTRID), "-", 1);
+	strftime(clTRID + strlen(clTRID), 17, "%Y%m%dT%H%M%S.", dateTime);
+	snprintf(clTRID + strlen(clTRID), 10, "%ld", unixTime.tv_nsec);
+	memcpy(clTRID + strlen(clTRID), "-", 1);
+	memcpy(clTRID + strlen(clTRID), client, 2);
+	memcpy(clTRID + strlen(clTRID), "-", 1);
+	memcpy(clTRID + strlen(clTRID), priority, 2);
+	memcpy(clTRID + strlen(clTRID), "-", 1);
+	memcpy(clTRID + strlen(clTRID), registrantID, strlen(registrantID));
 }
